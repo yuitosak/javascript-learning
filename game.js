5da1c6372491482114207f80eca7e786038135f2
Sakurai <skriyit@gmail.com>
2025-02-19 15:50:59 +0900

    ゲームの基本設定を追加

---
+++ game.js
@@ -0,0 +1,23 @@
+//プレイヤー情報
+let player={
+    name:"勇者",
+    health:100,
+    attack:10,
+    defense:5,
+};
+
+//敵キャラクター情報
+let enemy={
+    name:"スライム",
+    health:25,
+    attack:5
+};
+
+//ゲームタイトル
+const geameTitle="勇者の冒険";
+
+//現在のゲーム状況を表示
+console.log("=== ",gemeTitle+" ===");
+console.log("プレイヤー情報：",player);
+console.log("敵キャラクター情報：",enemy);
+
