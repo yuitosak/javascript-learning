a29d002d29dff4adb7791c5cb28f1a34531dc3b9
Sakurai <skriyit@gmail.com>
2025-02-25 15:46:49 +0900

    ゲームの基本設定を追加

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ let player={
     health:100,
     attack:10,
     defense:5,
+    level:1
 };
 
 //敵キャラクター情報
@@ -14,10 +15,14 @@ let enemy={
 };
 
 //ゲームタイトル
-const geameTitle="勇者の冒険";
+const gameTitle="勇者の冒険";
 
 //現在のゲーム状況を表示
-console.log("=== ",gemeTitle+" ===");
+console.log("=== "+ gameTitle+ " ===");
 console.log("プレイヤー情報：",player);
 console.log("敵キャラクター情報：",enemy);
+
+console.log(typeof player.name);  // 何が表示される？
+console.log(typeof player.health);
+console.log(typeof gameTitle);
 
